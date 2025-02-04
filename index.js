734f22d10e7961a889480fed0c55cde0b3091297
Devansh Bajpai <devanshb.code@gmail.com>
2025-02-04 21:55:09 +0530

    added basic server files

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+const { createServer } = require('node:http');
+const { Server } = require("socket.io");
+const server = createServer(app);
+const io = new Server(server);
+
+const path = require('path');
+const cookieParser = require('cookie-parser');
+
+
+var bcrypt = require('bcryptjs');
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+
+app.listen(3000);
