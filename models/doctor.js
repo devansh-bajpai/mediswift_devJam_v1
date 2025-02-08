92dbc4365590d932f51dc896f9dc76d89ee074dd
Devansh Bajpai <devanshb.code@gmail.com>
2025-02-08 07:21:03 +0530

    ..

--- models/doctor.js
+++ models/doctor.js
@@ -4,11 +4,18 @@ mongoose.connect('mongodb://127.0.0.1:27017/mediswift');
 
 const userData = mongoose.Schema({
     name: String,
+    email:String,
     institute: String,
     field: String,
     city: String,
     language: String,
-    experience: String
+    experience: String,
+    img: {
+        data: Buffer,
+        contentType: String
+    },
+    password: String,
+    desc: String
 })
 
 module.exports = mongoose.model("doctor", userData);
